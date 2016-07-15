00a1f79ff90df9e964b6c2a7a5cc4f1fbd4e0fc5
Andrew Maraev <the_vk@thevk.net>
2016-07-15 17:55:49 +0400

    Replaced lwrp sync with custom resource sync.

--- providers/sync.rb
+++
@@ -1,5 +0,0 @@
-action :sync do
-	sync_dirs(new_resource.source, new_resource.destination)
-	new_resource.updated_by_last_action(true)
-end
-
--- resources/sync.rb
+++ resources/sync.rb
@@ -3,3 +3,9 @@ default_action :sync
 
 attribute :source, kind_of: String
 attribute :destination, kind_of: String, name_attribute: true
+
+action_sync do
+    converge_by("sync from #{new_resource.source} to #{new_resource.destination}") do
+        sync_dirs(new_resource.source, new_resource.destination)
+    end
+end
